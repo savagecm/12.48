183b9e81912c2af926ca8065168e4c89488638d5
max cong <savagecm@qq.com>
2020-08-03 06:25:41 +0100

    a

--- include/ePaper/guiPaint.hpp
+++ include/ePaper/guiPaint.hpp
@@ -248,8 +248,10 @@ parameter:
             UBYTE Rdata = Paint.Image[Addr];
             // for black need to set the bit to 1
             Paint.Image[Addr] = Rdata | (0x80 >> (X % 8));
+
+              UBYTE RRdata = Paint.RImage[Addr];
              // for white need to set the bit to 0
-            Paint.RImage[Addr] = Rdata & ~(0x80 >> (X % 8));
+            Paint.RImage[Addr] = RRdata & ~(0x80 >> (X % 8));
         }
         else if (Color == RED)
         {
@@ -262,8 +264,10 @@ parameter:
             UBYTE Rdata = Paint.Image[Addr];
             // for white need to set the bit to 0
             Paint.Image[Addr] = Rdata & ~(0x80 >> (X % 8));
+              UBYTE RRdata = Paint.RImage[Addr];
              // for white need to set the bit to 0
-            Paint.RImage[Addr] = Rdata & ~(0x80 >> (X % 8));
+            Paint.RImage[Addr] = RRdata & ~(0x80 >> (X % 8));
+             
         }
         else
         {
